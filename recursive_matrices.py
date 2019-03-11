fixing an indexing bug in the recursive matrices computation

recursive_matrices.py
@@ -25,23 +25,30 @@ def compute_recursive_matrices(N, T, h, g):
     P_vu = np.zeros((N,N))
     P_zu = np.zeros((N,N))
 
-    temp_pu = np.zeros((1,N))
-    temp_vu = np.zeros((1,N))
-    temp_zu = np.zeros((1,N))
+    temp_pu = np.zeros((N))
+    temp_vu = np.zeros((N))
+    temp_zu = np.zeros((N))
 
     for i in range(N):
-         P_ps[i, 0:N] = np.array([1.0, (i+1)*T, 0.5*((i+1.0)*T)**2.0])
-         P_zs[i, 0:N] = np.array([1.0, (i+1)*T, 0.5*(((i+1.0)*T)**2.0) - h/g])
-         P_vs[i, 0:N] = np.array([0.0,      1.0 , (i+1.0)*T])
+        P_ps[i, 0:3] = np.array([1.0, (i+1.0)*T, 0.5*((i+1.0)*T)**2.0])
+        P_vs[i, 0:3] = np.array([0.0,      1.0 , (i+1.0)*T])
+        P_zs[i, 0:3] = np.array([1.0, (i+1.0)*T, 0.5*(((i+1.0)*T)**2.0) - h/g])
 
-         temp_pu[0, i] = np.array([(1.0 + (3.0*i) + 3.0*(i**2.0))*(T**3.0)/6.0])
-         temp_vu[0, i] = np.array([(1.0 + (2.0*i)) *(T**2.0)*0.5])
-         temp_zu[0, i] = np.array([((1.0 + (3.0*i) + 3.0*(i**2.0))*(T**3.0)/6.0) - T*(h/g)])
+        if i == 0:
+            temp_pu[i] = np.array([(T**3.0)/6.0])
+            temp_vu[i] = np.array([(T**2.0)*0.5])
+            temp_zu[i] = np.array([((T**3.0)/6.0) -T*h/g])
+        else:
+            temp_pu[i] = np.array([(1.0 + (3.0*(i+1.0)) + 3.0*((i+1.0)**2.0))*(T**3.0)/6.0])
+            temp_vu[i] = np.array([(1.0 + (2.0*(i+1.0))) *(T**2.0)*0.5])
+            temp_zu[i] = np.array([(1.0 + (3.0*(i+1.0)) + 3.0*((i+1.0)**2.0))*((T**3.0)/6.0) -T*h/g])
 
-    P_pu = toeplitz(temp_pu) * np.tri(N,N)
-    P_vu = toeplitz(temp_vu) * np.tri(N,N)
-    P_zu = toeplitz(temp_zu) * np.tri(N,N)
-
+        #print temp_pu
+        #print temp_vu
+        #print temp_zu
+        P_pu = toeplitz(temp_pu) * np.tri(N,N)
+        P_vu = toeplitz(temp_vu) * np.tri(N,N)
+        P_zu = toeplitz(temp_zu) * np.tri(N,N)
     ## Always check your matrices because karma is a fucking bitch !
     #print P_ps
     #print P_zs.shape