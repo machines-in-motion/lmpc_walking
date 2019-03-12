e06bfaedd9daec91c12d77fb23185a3e94452939
ahmadgazar <gazar.1692741@studenti.uniroma1.it>
2019-03-12 09:49:31 +0100

    code cleanup

--- closed_loop.py
+++ closed_loop.py
@@ -14,8 +14,8 @@ import plot_utils
 
 # regularization terms in the cost function:
 # -----------------------------------------
-alpha       = 10**(-5)
-gamma       = 10**(-1)
+alpha       = 10**(-6)
+gamma       = 0*10**(-6)
 
 # regularization term for Q
 Q_reg       = 0*10**(-10)
@@ -28,7 +28,7 @@ y_hat_0 = np.array([-0.09, 0.0, 0.0])
 
 # Inverted pendulum parameters:
 # ----------------------------
-h           = 0.75
+h           = 0.8
 g           = 9.81
 foot_length = 0.2
 foot_width  = 0.1
@@ -38,8 +38,8 @@ foot_width  = 0.1
 T                     = 0.1                        # sampling time interval
 step_time             = 0.8                        # time needed for every step
 no_steps_per_T        = int(round(step_time/T))
-no_desired_steps      = 5                                 # number of desired walking steps
-no_planned_steps      = 10*no_desired_steps                 # making sure to plan 2 steps ahead
+no_desired_steps      = 3                                 # number of desired walking steps
+no_planned_steps      = 2+no_desired_steps                 # making sure to plan 2 steps ahead
 desired_walking_time  = no_desired_steps * no_steps_per_T  # desired walking duration
 planned_walking_time  = no_planned_steps * no_steps_per_T  #
 N                     = 16                                 # preceding horizon
@@ -56,7 +56,10 @@ desired_Z_ref = reference_trajectories.create_CoP_trajectory(no_desired_steps, d
 planned_Z_ref = reference_trajectories.create_CoP_trajectory(no_planned_steps, plannedFoot_steps, \
                                                 planned_walking_time, no_steps_per_T)
 
-[P_ps, P_vs, P_zs, P_pu, P_vu, P_zu] = recursive_matrices.compute_recursive_matrices(N, T, h, g)
+#print 'desired_Z_ref', desired_Z_ref, '\n'
+#print 'planned_Z_ref', planned_Z_ref, '\n'
+#planned_Z_ref[desired_walking_time:planned_walking_time,:] = desired_Z_ref[desired_walking_time-1,:]
+#print planned_Z_ref
 
 x_hat_k   = x_hat_0
 y_hat_k   = y_hat_0
@@ -72,6 +75,8 @@ Y_total   = np.zeros((desired_walking_time,3))
 Z_x_total = np.zeros((desired_walking_time))
 Z_y_total = np.zeros((desired_walking_time))
 
+[P_ps, P_vs, P_as, P_zs, P_pu, P_vu, P_au, P_zu] = recursive_matrices.compute_recursive_matrices(N, T, h, g)
+
 for i in range(desired_walking_time):
 
     [Q, p_k] = cost_function.compute_objective_terms(alpha, gamma, N, P_zs,\
@@ -84,9 +89,9 @@ for i in range(desired_walking_time):
     current_U = solve_qp(Q, -p_k, A.T, b)[0]
 
     # evaluate your recursive dynamics over the current horizon:
-    [X_k, Y_k, Z_x_k, Z_y_k] = motionModel.compute_recursive_dynamics(P_ps,\
-                                            P_vs, P_zs, P_pu, P_vu, P_zu, N,\
-                                            x_hat_k, y_hat_k, current_U)
+    [X_k, Y_k, Z_x_k, Z_y_k] = motionModel.compute_recursive_dynamics(P_ps, P_vs, P_as, P_zs, P_pu, \
+                                                            P_vu, P_au, P_zu, N, \
+                                                            x_hat_k, y_hat_k, current_U)
     # update the next state
     X_total[i,:]  = X_k[0,:]
     Y_total[i,:]  = Y_k[0,:]
@@ -98,26 +103,19 @@ for i in range(desired_walking_time):
     y_hat_k   = Y_k[0,:]                    # update the next y state for next iteration
     Z_ref_k   = planned_Z_ref[i+1:i+N+1,:]  # update cop references for next iteration
 
-
 # ------------------------------------------------------------------------------
 # debugging:
 # ------------------------------------------------------------------------------
-#Te=np.arange(0,N,1)
-#plt.figure()
-#plt.plot(Te,Z_ref_k[:,1])
-#print 'Z_ref_k = ', Z_ref_k
+    #print 'x_hat_k = ', x_hat_k[0], '\n'
+    #print 'x_hatdot_k = ', X_k[0,1], '\n'
+    #print 'x_hatddot_k = ', X_k[0,2], '\n'
 
-#print 'Z_x_k = ', Z_x_k[0], '\n'
-#print 'X_k = ', X_k[0:N,0], '\n'
-#print 'x_hat_k = ', X_k[0,0], '\n'
-#print 'x_hatdot_k = ', X_k[0,1], '\n'
-#print 'x_hatddot_k = ', X_k[0,2], '\n'
+    #print 'y_hat_k = ', y_hat_k[0], '\n'
+    #print 'y_hatdot_k = ', Y_k[0,1], '\n'
+    #print 'y_hatddot_k = ', Y_k[0,2], '\n'
 
-#print 'Z_y_k = ', Z_y_k[0], '\n'
-#print 'Y_k = ', Y_k[0:N,0], '\n'
-#print 'y_hat_k = ', Y_k[0,0], '\n'
-#print 'y_hatdot_k = ', Y_k[0,1], '\n'
-#print 'y_hatddot_k = ', Y_k[0,2], '\n'
+    #print 'Z_x_k = ', Z_x_k[0], '\n'
+    #print 'Z_y_k = ', Z_y_k[0], '\n'
 
 # ------------------------------------------------------------------------------
 # visualize:
