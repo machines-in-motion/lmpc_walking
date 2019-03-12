688a1eb3823e01421ba17fb8fc1fddb9e0898544
ahmadgazar <gazar.1692741@studenti.uniroma1.it>
2019-03-12 08:34:14 +0100

    fixing acceleration recursion bug

--- motionModel.py
+++ motionModel.py
@@ -8,18 +8,18 @@ import constraints
 # x_k, y_k and control inputs U
 # ------------------------------------------------------------------------------
 
-def compute_recursive_dynamics(P_ps, P_vs, P_zs, P_pu, P_vu, P_zu, N, x_hat_k, y_hat_k, U):
+def compute_recursive_dynamics(P_ps, P_vs, P_as, P_zs, P_pu, P_vu, P_au, P_zu, N, x_hat_k, y_hat_k, U):
     X         = np.zeros((N,3))
     Y         = np.zeros((N,3))
 
     # evaluate your states over the horizon
     X[0:N,0]  = np.dot(P_ps, x_hat_k) + np.dot(P_pu, U[0:N])   #x
     X[0:N,1]  = np.dot(P_vs, x_hat_k) + np.dot(P_vu, U[0:N])   #x_dot
-    X[0:N,2]  = np.dot(P_zs, x_hat_k) + np.dot(P_zu, U[0:N])   #x_ddot
+    X[0:N,2]  = np.dot(P_as, x_hat_k) + np.dot(P_au, U[0:N])   #x_ddot
 
     Y[0:N,0]  = np.dot(P_ps, y_hat_k) + np.dot(P_pu, U[N:2*N]) #y
     Y[0:N,1]  = np.dot(P_vs, y_hat_k) + np.dot(P_vu, U[N:2*N]) #y_Dot
-    Y[0:N,2]  = np.dot(P_zs, y_hat_k) + np.dot(P_zu, U[N:2*N]) #y_ddot
+    Y[0:N,2]  = np.dot(P_as, y_hat_k) + np.dot(P_au, U[N:2*N]) #y_ddot
 
     # evaluate actual CoP
     Z_x = np.zeros((N))
--- recursive_matrices.py
+++ recursive_matrices.py
@@ -19,28 +19,35 @@ from scipy.linalg import toeplitz
 def compute_recursive_matrices(N, T, h, g):
     P_ps = np.zeros((N,3))
     P_vs = np.zeros((N,3))
+    P_as = np.zeros((N,3))
     P_zs = np.zeros((N,3))
 
     P_pu = np.zeros((N,N))
     P_vu = np.zeros((N,N))
+    P_au = np.zeros((N,N))
+
     P_zu = np.zeros((N,N))
 
     temp_pu = np.zeros((N))
     temp_vu = np.zeros((N))
+    temp_au = np.zeros((N))
     temp_zu = np.zeros((N))
 
     for i in range(N):
         P_ps[i, 0:3] = np.array([1.0, (i+1.0)*T, 0.5*((i+1.0)*T)**2.0])
         P_vs[i, 0:3] = np.array([0.0,      1.0 , (i+1.0)*T])
+        P_as[i, 0:3] = np.array([0.0,      0.0 , 1.0])
         P_zs[i, 0:3] = np.array([1.0, (i+1.0)*T, 0.5*(((i+1.0)*T)**2.0) - h/g])
 
         if i == 0:
             temp_pu[i] = np.array([(T**3.0)/6.0])
             temp_vu[i] = np.array([(T**2.0)*0.5])
+            temp_au[i] = np.array([T])
             temp_zu[i] = np.array([((T**3.0)/6.0) -T*h/g])
         else:
             temp_pu[i] = np.array([(1.0 + (3.0*(i+1.0)) + 3.0*((i+1.0)**2.0))*(T**3.0)/6.0])
             temp_vu[i] = np.array([(1.0 + (2.0*(i+1.0))) *(T**2.0)*0.5])
+            temp_au[i] = np.array([T])
             temp_zu[i] = np.array([(1.0 + (3.0*(i+1.0)) + 3.0*((i+1.0)**2.0))*((T**3.0)/6.0) -T*h/g])
 
         #print temp_pu
@@ -48,6 +55,7 @@ def compute_recursive_matrices(N, T, h, g):
         #print temp_zu
         P_pu = toeplitz(temp_pu) * np.tri(N,N)
         P_vu = toeplitz(temp_vu) * np.tri(N,N)
+        P_au = toeplitz(temp_au) * np.tri(N,N)
         P_zu = toeplitz(temp_zu) * np.tri(N,N)
     ## Always check your matrices because karma is a fucking bitch !
     #print P_ps
@@ -58,4 +66,4 @@ def compute_recursive_matrices(N, T, h, g):
     #print P_vu
     #print P_zu
 
-    return P_ps, P_vs, P_zs, P_pu, P_vu, P_zu
+    return P_ps, P_vs, P_as, P_zs, P_pu, P_vu, P_au, P_zu
