b1b69d5ea785e7a85fb1c87a906c59cf6ada1828
ahmadgazar <gazar.1692741@studenti.uniroma1.it>
2019-03-12 09:44:47 +0100

    renaming open_loop.py to trajectory_planning.py

--- trajectory_planning.py
+++ trajectory_planning.py
@@ -14,11 +14,11 @@ import plot_utils
 
 # regularization terms in the cost function:
 # -----------------------------------------
-alpha = 0*10**(-1)
-gamma = 10**(-6)
+alpha = 10**(-6)
+gamma = 0*10**(-6)
 
 # regularization term for Q
-Q_reg = 10**(-10)
+Q_reg = 0*10**(-10)
 
 # CoM initial state: [x, xdot, x_ddot].T
 #                    [y, ydot, y_ddot].T
@@ -28,7 +28,7 @@ y_hat_0 = np.array([-0.09, 0.0, 0.0])
 
 # Inverted pendulum parameters:
 # ----------------------------
-h           = 0.75
+h           = 0.8
 g           = 9.81
 foot_length = 0.2
 foot_width  = 0.1
@@ -38,10 +38,8 @@ foot_width  = 0.1
 T                = 0.1                        # sampling time interval
 step_time        = 0.8                        # time needed for every step
 no_steps_per_T   = int(round(step_time/T))
-no_steps         = 4                         # number of desired walking steps
+no_steps         = 3                          # number of desired walking steps
 walking_time     = no_steps * no_steps_per_T  # number of sampling time intervals
-N                = walking_time
-
 
 # compute CoP reference trajectory:
 # --------------------------------
@@ -49,18 +47,19 @@ foot_step_0 = np.array([0.0, -0.09])     # initial foot step position in x-y
 Foot_steps  = reference_trajectories.manual_foot_placement(foot_step_0, no_steps)
 Z_ref  = reference_trajectories.create_CoP_trajectory(no_steps, Foot_steps, \
                                                       walking_time, no_steps_per_T)
+#print Z_ref
 #plt.plot(Z_ref[:,0], Z_ref[:,1])
 #plt.show()
 
 # construct your preview system: 'Go pokemon !'
 # --------------------------------------------
-[P_ps, P_vs, P_zs, P_pu, P_vu, P_zu] = recursive_matrices.compute_recursive_matrices(N, T, h, g)
+[P_ps, P_vs, P_as, P_zs, P_pu, P_vu, P_au, P_zu] = recursive_matrices.compute_recursive_matrices(walking_time, T, h, g)
 [Q, p_k] = cost_function.compute_objective_terms(alpha, gamma, walking_time, \
                                                  P_zs, P_zu, x_hat_0, y_hat_0,\
                                                  Z_ref)
 [A, b]   = constraints.add_ZMP_constraints(walking_time, foot_length, foot_width,\
                                           P_zs, P_zu, Z_ref, x_hat_0, y_hat_0)
-Q        = Q_reg*np.identity(2*N) + Q   #making sure that Q is +ve definite
+Q        = Q_reg*np.identity(2*walking_time) + Q   #making sure that Q is +ve definite
 
 # call the solver del sto cazzo:
 # -----------------------------
@@ -68,29 +67,24 @@ U = solve_qp(Q, -p_k, A.T, b)[0]
 
 # open-loop planning: (based on the initial state x_hat_0, y_hat_0)
 # -------------------------------------------------------------------------
-[X, Y, Z_x, Z_y] = motionModel.compute_recursive_dynamics(P_ps, P_vs, P_zs, P_pu, \
-                                                        P_vu, P_zu, walking_time, \
+[X, Y, Z_x, Z_y] = motionModel.compute_recursive_dynamics(P_ps, P_vs, P_as, P_zs, P_pu, \
+                                                        P_vu, P_au, P_zu, walking_time, \
                                                         x_hat_0, y_hat_0, U)
 
-# -------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # debugging:
-# -------------------------------------------------------------------------
-#print 'x = ', X[:,0], '\n'
-#print 'y = ', Y[:,0], '\n'
+# ------------------------------------------------------------------------------
+#for i in range (2):
+#    print 'x_hat_k = ', X[i,0], '\n'
+#    print 'x_hatdot_k = ', X[i,1], '\n'
+#    print 'x_hatddot_k = ', X[i,2], '\n'
 
-#print 'Z_x = ', Z_x, '\n'
-#for i in range (Z_y.shape[0]):
-    #print 'x_hat_k = ', X[i,0], '\n'
-    #print 'y_hat_k = ', Y[i,0], '\n'
+#    print 'y_hat_k = ', Y[i,0], '\n'
+#    print 'y_hatdot_k = ', Y[i,1], '\n'
+#    print 'y_hatddot_k = ', Y[i,2], '\n'
 
-    #print 'x_hatdot_k = ', X[i,1], '\n'
-    #print 'x_hatddot_k = ', X[i,2], '\n'
-
-    #print 'y_hatdot_k = ', Y[i,1], '\n'
-    #print 'y_hatddot_k = ', Y[i,2], '\n'
-
-    #print 'Z_x_k = ', Z_x[i], '\n'
-    #print 'Z_y_k = ', Z_y[i], '\n'
+#    print 'Z_x_k = ', Z_x[i], '\n'
+#    print 'Z_y_k = ', Z_y[i], '\n'
 
 # ------------------------------------------------------------------------------
 # visualize:
